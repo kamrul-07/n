2cf3f945fa609f73cb25166db5182d5914c6811a
kamrul-07 <kamrul.i.akib@gmail.com>
2021-08-21 18:08:25 +0600

    first-commit

---
+++ j.js
@@ -0,0 +1,6 @@
+document.getElementById('t').addEventListener('click',function(){
+    window.location.href="h.html"
+})
+document.getElementById('f').addEventListener('click',function(){
+    window.location.href="t.html"
+})
